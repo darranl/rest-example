b81aedc588b6ed51a7bd01db013589bbc2c6366f
Darran Lofthouse <darran.lofthouse@jboss.com>
2023-02-18 11:43:07 +0000

    Add a simple "Hello X" restful endpoint.

---
+++ src/main/java/org/wildfly/security/examples/LoginFormApplication.java
@@ -0,0 +1,9 @@
+package org.wildfly.security.examples;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/hello")
+public class LoginFormApplication extends Application {
+
+}
